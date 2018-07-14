518911166057752fa929b869f3480959d1c23eb5
Babs Craig <babscraig@gmail.com>
2018-07-14 10:31:04 +0100

    add reducers for user data and ticker data

--- src/reducers/tickerReducer.js
+++ src/reducers/tickerReducer.js
@@ -1,14 +1,37 @@
-// import { } from '../actions/types';
+import {
+  FETCH_LAST_BTC_PRICE_SUCCESS,
+  FETCH_LAST_BTC_PRICE_LOADING,
+  FETCH_LAST_BTC_PRICE_ERROR
+} from '../constants';
 
 const initialState = {
   loading: false,
-  last_price: null,
-  timestamp: null
+  lastPrice: null,
+  timestamp: null,
+  errorMsg: ""
 }
 
-export default function (state = initialState, action) {
-  switch (action.type) {
-
+export default function (state = initialState, {type, payload}) {
+  switch (type) {
+    case FETCH_LAST_BTC_PRICE_LOADING:
+      return {
+        ...state,
+        loading: true
+      }
+    case FETCH_LAST_BTC_PRICE_SUCCESS:
+      const { timestamp, lastPrice } = payload;
+      return {
+        ...state,
+        lastPrice,
+        timestamp,
+        loading: false
+      }
+    case FETCH_LAST_BTC_PRICE_ERROR:
+      return {
+        ...state,
+        errorMsg: "There was an error fetching price data. Please try again",
+        loading: false
+      }
     default:
       return state;
   }
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,10 +1,12 @@
 // import { } from '../actions/types';
 
 const initialState = {
-  firstName: "Babs",
-  lastName: "Craig",
-  usdBalance: 156.12,
-  btcBalance: 0
+  user: {
+    firstName: "Babs",
+    lastName: "Craig",
+    usdBalance: 156.12,
+    btcBalance: 0
+  }
 }
 
 export default function (state = initialState, action) {
