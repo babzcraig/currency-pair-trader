ebabefadc5b786df0b227ad781b28bb924ed66a1
Babs Craig <babscraig@gmail.com>
2018-07-16 00:27:05 +0100

    Fix styling for both spinners and textbased loaders

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,5 @@
 const BTC_API_URL = 'https://api.bitfinex.com/v2/ticker/tBTCUSD';
+// we will poll the API every 15 seconds for an update
+const POLL_INTERVAL_IN_MS = 15000;
 
-export {BTC_API_URL};
+export {BTC_API_URL, POLL_INTERVAL_IN_MS};
